07cbd94c37c842ce9a744fb8a95b7d521c4ac3df
George Raju <georgeraju@gmail.com>
2025-04-14 16:55:17 +0200

    Attempt 2 at assignment 3 part2, ./finder-app/finder-test.sh updated to refer to rootfs conf file.

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -9,7 +9,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat ./conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -33,7 +33,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=`cat ./conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
