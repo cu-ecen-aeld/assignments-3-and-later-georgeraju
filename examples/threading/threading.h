4b460db156cab9f6903da83fccd2ff919fe4e8de
George Raju <georgeraju@gmail.com>
2025-06-09 14:58:06 +0200

    Assignment 4 part 1 - Threading with Mutex

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -19,6 +19,14 @@ struct thread_data{
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
+    pthread_mutex_t *mutex;
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
+
+    /**
+     * Set to true if the thread completed with success, false
+     * if an error occurred.
+     */
     bool thread_complete_success;
 };
 
